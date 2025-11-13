cf564054312b1a10072669cb4e3fa909e78a2b45
matyassvoboda <matysvoboda.ra@gmail.com>
2025-11-13 20:02:51 +0100

    installing moduls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxt/icon",
+    "@nuxt/image"
+  ],
+
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [".index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6347",
+        "dodgeroll-gold": "#F789FA1",
+        "apple-green": "#046E18",
+        "dire-wolf": "292727",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  plugins: [],
+}
+
