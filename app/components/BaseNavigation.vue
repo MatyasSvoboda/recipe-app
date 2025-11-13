b1a49dd05a0daba65a21f4e8c1ffbb4abb8da7bf
matyassvoboda <matysvoboda.ra@gmail.com>
2025-11-13 21:42:20 +0100

    About section

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -3,10 +3,16 @@
 <template>
   <header class="py-4 border-b">
     <nav class="flex container items-center">
+      <NuxtLink to="/" class="text-2xl font-bold">
+        <span class="text-3xl font-bold">Recipe</span>
+      </NuxtLink>
         <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
-            <li>
-                <a href="">Home</a>
-            </li>
+              <li>
+                  <NuxtLink to="/">Home</NuxtLink>
+              </li>
+              <li>
+                  <NuxtLink to="/about">About</NuxtLink>
+              </li>
         </ul>
     </nav>
   </header>
---
+++ app/pages/about.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <main>
+        <section class="container py-20">
+            <h1 class="text-6xl font-extrabold mb-6 text-balance">About this app</h1>
+            <p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+                This app is a simple recipe app that allows you to search for recipes by name or ingredients.
+            </p>
+        </section>
+    </main>
+  </div>
+</template>
