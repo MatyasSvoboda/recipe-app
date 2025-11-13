bc4ecc164099458efd255c482ac62c2d712bd36f
matyassvoboda <matysvoboda.ra@gmail.com>
2025-11-13 22:18:43 +0100

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,51 @@
 <script setup lang="ts">
-    
+    const { data, error } = await useFetch('https://dummyjson.com/recipes?limit=12')
 </script>
 <template>
-  <div>
-    <h1>Home page</h1>
-  </div>
+  <main>
+    <section class="bg-[#f1f1f1]">
+      <div class="container flex flex-col lg:flex-row items-center py-20 gap-10">
+        <div class="flex-1 order-2 lg:order-1 text-center lg:text-left">
+          <h1 class="text-4xl lg:text-6xl font-extrabold mb-6 text-balance">
+            Master the Kitchen with Ease: Unleash Your Inner Chef Today!
+          </h1>
+          <p class="text-xl lg:text-2xl mb-8 text-balance max-w-screen-lg">
+            Discover a world of delicious recipes, step-by-step guides, and expert tips. Whether you're a beginner or a seasoned chef, we've got you covered.
+          </p>
+          <button class="px-4 py-2 text-white selft-start bg-dodgeroll-gold rounded-md text-lg cursor-pointer">
+            Browse Recipes
+          </button>
+        </div>
+        <div class="flex-1 order-1 lg:order-2">
+
+        </div>
+      </div>
+    </section>
+    <section class="py-20 container">
+      <h2 class="teyt-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
+      <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes</p>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        <div class="flex flex-col shadow rounded-md">
+          <img src="" alt="" class="rounded-t-md" />
+          <div class="flex flex-col py-6 px-4 flex-1">
+            <p class="text-xl lg:text-2xl font-semibold mb-2">Recipe Name</p>
+            <div class="font-normal w-full bg-white/80 flex gap-8 text-lg lg:text-xl mb-4 mt-auto">
+              <div class="flex items-center gap-1">
+                <Icon name="mdi:clock-time-eight-outline" style="color: #f79f1a" />
+                <span>Cook Time</span>
+              </div>
+              <div class="flex items-center gap-1">
+                <Icon name="mdi:fire" style="color: #f79f1a" />
+                <span>Calories</span>
+              </div>
+              <div class="flex items-center gap-1">
+                <Icon name="mdi:star" style="color: #f79f1a" />
+                <span>Rating (Rating Count)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
