266bcc6ae29460f7ecda3d81dbd709c357f3976c
matyassvoboda <matysvoboda.ra@gmail.com>
2025-11-13 20:28:15 +0100

    Start adding components and layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="font-sans container">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/components/BaseNavigation.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+        <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+            <li>
+                <a href="">Home</a>
+            </li>
+        </ul>
+    </nav>
+  </header>
+</template>
